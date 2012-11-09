e49c8904ee382968287c5a7d0b5a31fd8351eabd
Pushparaj <pushparaj@comogroupap.com>
2012-11-09 16:52:21 +0530

    jobeet weekend commit

---
+++ src/Push/JobeetBundle/Controller/JobController.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Push\JobeetBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Push\JobeetBundle\Entity\Job;
+use Push\JobeetBundle\Form\JobType;
+
+/**
+ * Job controller.
+ *
+ */
+class JobController extends Controller
+{
+    /**
+     * Lists all Job entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+          $categories = $em->getRepository('PushJobeetBundle:Category')->getWithJobs();
+//        $entities = $em->getRepository('PushJobeetBundle:Job')->getActiveJobs();;
+//        $query = $em->createQuery(
+//        'SELECT j FROM PushJobeetBundle:Job j WHERE j.created_at > :date'
+//        )->setParameter('date', date('Y-m-d H:i:s', time() - 86400 * 30));
+//        $entities = $query->getResult();
+//        echo date('Y-m-d H:i:s', time() - 86400 * 30); exit;.
+        foreach($categories as $category)
+        {
+            $category->setActiveJobs($em->getRepository('PushJobeetBundle:Job')->getActiveJobs($category->getId(), $this->container->getParameter('max_jobs_on_homepage')));
+        }
+        return $this->render('PushJobeetBundle:Job:index.html.twig', array(
+//            'entities' => $entities,
+             'categories' => $categories
+        ));
+    }
+
+    /**
+     * Finds and displays a Job entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PushJobeetBundle:Job')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Job entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('PushJobeetBundle:Job:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),        ));
+    }
+
+    /**
+     * Displays a form to create a new Job entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Job();
+        $form   = $this->createForm(new JobType(), $entity);
+
+        return $this->render('PushJobeetBundle:Job:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a new Job entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Job();
+        $form = $this->createForm(new JobType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('push_job_show', array('id' => $entity->getId(),'company'=> $entity->getcompany(), 'location'=> $entity->getlocation(), 'position'=> $entity->getposition())));
+        }
+
+        return $this->render('PushJobeetBundle:Job:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Job entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PushJobeetBundle:Job')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Job entity.');
+        }
+
+        $editForm = $this->createForm(new JobType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('PushJobeetBundle:Job:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Edits an existing Job entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PushJobeetBundle:Job')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Job entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new JobType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('push_job_edit', array('id' => $id)));
+        }
+
+        return $this->render('PushJobeetBundle:Job:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Job entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('PushJobeetBundle:Job')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Job entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('push_job'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
